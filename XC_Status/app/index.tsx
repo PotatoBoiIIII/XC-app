aa7c90e5c9652e5af57258bcde9bf26ad2b521c6
Liam R Zadoorian <41171966+XC4Life101@users.noreply.github.com>
2024-09-10 20:27:30 +0000

    meh

--- XC_Status/app/index.tsx
+++ XC_Status/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>This is where all of the Profiles will be</Text>
     </View>
   );
 }
