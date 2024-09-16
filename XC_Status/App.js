894b1e61f7a556adbd02feefe414ee1ff872e564
Luke Yen <luksogami@icloud.com>
2024-09-15 18:46:19 -0700

    exploring

--- XC_Status/App.js
+++ XC_Status/App.js
@@ -1,12 +1,48 @@
+import * as React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const image1 = require('./assets/images/image1.png')
+const image2 = require('./assets/images/image2.png')
+const Tab = createBottomTabNavigator()
+
+
+function Status(){
+  return(
+    <View style ={styles.container}>
+      <Image style={styles.image} source = {image1}>
+
+      </Image>
+    </View>
+      
+   
+  )
+}
+
+function Settings(){
+  return(
+    <View style={styles.container}>
+      <Image style={styles.image} source = {image2}>
+
+      </Image>
+    </View>
+      
+   
+  )
+}
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="status" component={Status} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,4 +53,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    width: 320,
+    height: 440,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  }
 });
