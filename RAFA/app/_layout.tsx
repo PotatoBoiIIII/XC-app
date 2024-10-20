38d5e6d63e93d5782c454ad6e7290c663864e50e
Luke Yen <luksogami@icloud.com>
2024-10-19 19:56:04 -0700

    line

--- RAFA/app/_layout.tsx
+++ RAFA/app/_layout.tsx
@@ -207,7 +207,7 @@ const AthletesScreen = ()=>{
 
   return(
     <View style = {styles.container}>
-      <View style = {{flexDirection:'row', flex:1/10, alignItems:'center', justifyContent:'center'}}>
+      <View style = {{flexDirection:'row', flex:1/10, paddingHorizontal:20, alignItems:'center', borderBottomWidth:1, justifyContent:'center', shadowColor: '#171717', shadowOffset: {width: 0, height: 4}, shadowOpacity: 0.2, shadowRadius: 3,}}>
 
         {renderLabel()}
         <Dropdown
